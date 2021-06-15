7957dd1ec513dbfffec691689760ab151dd0244f
Dave Inoc <michael828inoc@gmail.com>
2021-06-15 13:57:11 +0800

    first part

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <input type="text" v-model="search" />
+    <p>search term - {{ search }}</p>
+    <div :key="name" v-for="name in matchingNames">
+      {{ name }}
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from "@vue/reactivity";
+import { computed, watch, watchEffect } from "@vue/runtime-core";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  setup() {
+    const search = ref("");
+    const names = ref(["mario", "john", "toad"]);
+
+    const stopWatch = watch(search, () => {
+      console.log(search.value);
+    });
+
+    const stopEffect = watchEffect(() => {
+      console.log("watchEffect function ran");
+    });
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+
+    return { names, search, matchingNames };
+  },
+};
 </script>
